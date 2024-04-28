b17dfaf9a93b0d57d2b09b947ef40bbd7f226f23
KhusnutdinovRustem <167807173+KhusnutdinovRustem@users.noreply.github.com>
2024-04-28 17:17:48 +0300

    Add files via upload

---
+++ Homework5.py
@@ -0,0 +1,14 @@
+my_list = ['apple', 'coconut', 'pineapple', 'peach', 'banana', 'kiwi', 'pear', 'mango']
+print(my_list)
+print('First element: ' + my_list[0])
+print('Last element: ' + my_list[-1])
+print(my_list[0::7])
+print(my_list[2:5])
+my_list[2] = 'pomegranate'
+print(my_list)
+
+my_dict = {'apple': 'яблоко', 'coconut': 'кокос', 'pineapple': 'ананас', 'peach': 'персик', 'banana': 'банан'}
+print(my_dict)
+print(my_dict['peach'])
+my_dict['apple'] = 'яблочко'
+print(my_dict)
